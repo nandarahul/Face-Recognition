c1080da861783d4b46e2f582bbdba07ad6eadfcc
Rahul <rahulnanda.iiti@gmail.com>
2018-11-23 21:50:20 -0500

    Script to create training-testing split

---
+++ create_test.py
@@ -0,0 +1,28 @@
+import os, shutil
+
+TRAINING_DIR_PATH, TEST_DIR_PATH= "train_dataset", "test_dataset"
+LFW_DIR_PATH = "lfw-deepfunneled"
+os.mkdir(TRAINING_DIR_PATH)
+os.mkdir(TEST_DIR_PATH)
+train_image_count = 0
+for label_directory_name in os.listdir(LFW_DIR_PATH):
+        label_directory_path = os.path.join(LFW_DIR_PATH, label_directory_name)
+        if not os.path.isdir(label_directory_path):
+            continue
+        image_count = len(os.listdir(label_directory_path))
+        if image_count < 5:
+            continue
+        training_label_dir_path, test_label_dir_path = os.path.join(TRAINING_DIR_PATH, label_directory_name), \
+                                                       os.path.join(TEST_DIR_PATH, label_directory_name)
+        print("Creating directory: {}".format(test_label_dir_path))
+        os.mkdir(training_label_dir_path)
+        os.mkdir(test_label_dir_path)
+        for i, image_name in enumerate(os.listdir(label_directory_path)):
+            if i < image_count * 0.8:
+                shutil.copy(os.path.join(label_directory_path, image_name), training_label_dir_path)
+                train_image_count += 1
+            else:
+                shutil.copy(os.path.join(label_directory_path, image_name), test_label_dir_path)
+        if train_image_count > 5000:
+            break
+print("Training image count: {}".format(train_image_count))
